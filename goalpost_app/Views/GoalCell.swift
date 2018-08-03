f1c2f4193b356efb8615c9663b4cbc58ee9d0815
Stephen Reyes <sjreyes1@yahoo.com>
2018-08-03 14:15:47 -0700

    Built goal cell

---
+++ goalpost_app/Views/GoalCell.swift
@@ -0,0 +1,25 @@
+//
+//  GoalCell.swift
+//  goalpost_app
+//
+//  Created by Stephen Reyes on 8/3/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+// outlets
+    @IBOutlet weak var goalDescriptionLbl: UILabel!
+    @IBOutlet weak var goalTypeLbl: UILabel!
+    @IBOutlet weak var goalProgressLbl: UILabel!
+    
+    
+    func configureCell(description: String, type: String, goalProgressAmount: Int){
+        self.goalDescriptionLbl.text = description
+        self.goalTypeLbl.text = type
+        self.goalProgressLbl.text = String(describing: goalProgressAmount)
+    }
+    
+}
