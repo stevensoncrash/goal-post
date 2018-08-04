16447b414cb1b6b823a700d5d7954eaf104c4108
Stephen Reyes <sjreyes1@yahoo.com>
2018-08-03 23:19:24 -0700

    Displaying Static GoalCells in UITableView / Creating GoalType Enum

--- goalpost_app/Controllers/GoalsVC.swift
+++ goalpost_app/Controllers/GoalsVC.swift
@@ -16,14 +16,11 @@ class GoalsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.isHidden = false 
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
   
     @IBAction func addGoalBtnWasPressed(_ sender: Any) {
         
@@ -32,3 +29,25 @@ class GoalsVC: UIViewController {
     
 }
 
+
+extension GoalsVC: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "goalCell") as? GoalCell else {
+            return UITableViewCell() }
+        cell.configureCell(description: "Workout 3 times a week.", type: .shortTerm, goalProgressAmount: 2)
+        return cell
+    }
+    
+    
+    
+    
+    
+}
---
+++ goalpost_app/Enums/GoalType.swift
@@ -0,0 +1,17 @@
+//
+//  GoalType.swift
+//  goalpost_app
+//
+//  Created by Stephen Reyes on 8/3/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import Foundation
+
+
+enum GoalType: String {
+    
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+    
+}
