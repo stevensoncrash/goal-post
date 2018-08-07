a5c4a31ce6e5b69d0a3137ad73666406403ea07a
Stephen Reyes <sjreyes1@yahoo.com>
2018-08-07 10:57:20 -0700

    built Create Goals VC

---
+++ goalpost_app/Controllers/CreateGoalVC.swift
@@ -0,0 +1,41 @@
+//
+//  CreateGoalVC.swift
+//  goalpost_app
+//
+//  Created by Stephen Reyes on 8/7/18.
+//  Copyright © 2018 Stephen Reyes. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    
+//outlets
+    
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+//actions
+    
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+}
